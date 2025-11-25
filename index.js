53ad46fa5219bcc1a00f3859c067ab062ef7108d
EmilieBathory <marvlarus@gmail.com>
2025-11-25 14:18:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import cors from 'cors';
 
 const app = express();
+app.use(cors()); // Разрешаем все домены
+
 const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
@@ -10,14 +13,12 @@ app.get('/', (req, res) => {
 
 app.get('/api/posts', async (req, res) => {
   const { id, count = 5, access_token } = req.query;
-
   if (!id || !access_token) return res.status(400).json({ error: "Укажите id и access_token" });
 
   try {
     const vkUrl = `https://api.vk.com/method/wall.get?owner_id=${id}&count=${count}&access_token=${access_token}&v=5.131`;
     const vkResponse = await fetch(vkUrl);
     const data = await vkResponse.json();
-
     if (data.error) return res.status(400).json({ error: data.error });
 
     res.json({ posts: data.response.items });
